115c18f017b31e087585b15cf47ae76c3474cb7e
RNtaate <royhyde14@gmail.com>
2020-09-08 18:01:26 +0300

    Update validation of password in user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 2..20}
   validates :email, presence: true, length: { in: 8..30 }, uniqueness: true, format: { with: /[\w]+@([\w-]+\.)+[\w-]{2,4}/,
   message: "not valid" }
-  validates :crypted_password, presence: true
+  validates :password, presence: true
+  validates_confirmation_of :password,  message: 'should match confirmation', if: :password
 end
