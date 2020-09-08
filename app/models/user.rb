4dd96182882c35f9b8bc3079d6d16d41667c584d
ershadul1 <ershadulrayhan@gmail.com>
2020-09-08 20:13:28 +0600

    Add validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :username, presence: true, length: { in: 2..20}
+  validates :email, presence: true, length: { in: 8..30 }, uniqueness: true, format: { with: /[\w]+@([\w-]+\.)+[\w-]{2,4}/,
+  message: "not valid" }
+  validates :crypted_password, presence: true
 end
