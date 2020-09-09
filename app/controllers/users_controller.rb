d1e6580aa79d01b74e8d13c23882bc223c893483
ershadul1 <ershadulrayhan@gmail.com>
2020-09-09 15:08:05 +0600

    Add rails form_tag form in new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    #@user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
   
     if @user.save
       redirect_to new_user_path
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
