2ad216f61b76f128d18cebefed045b327466940d
RNtaate <royhyde14@gmail.com>
2020-09-09 16:24:05 +0300

    Update update method in user_controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,16 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
   
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
   end
 
   private
